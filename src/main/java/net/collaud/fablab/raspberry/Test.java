7988bbb9213aca1981bc28074bc598cbab9d4d03
Gaétan Collaud <gaetan.collaud@gmail.com>
2014-05-06 19:00:24 +0200

    readme

---
+++ src/main/java/net/collaud/fablab/raspberry/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.collaud.fablab.raspberry;
+
+/**
+ *
+ * @author gaetan
+ */
+public class Test {
+	
+}
