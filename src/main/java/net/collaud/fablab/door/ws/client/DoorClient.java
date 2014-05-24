26f960f4f2d9f6c7e2e264b89410327818ccd19c
Gaétan Collaud <gaetan.collaud@gmail.com>
2014-05-24 23:39:03 +0200

    constants

--- src/main/java/net/collaud/fablab/door/ws/client/DoorClient.java
+++ src/main/java/net/collaud/fablab/door/ws/client/DoorClient.java
@@ -4,6 +4,7 @@ import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import net.collaud.fablab.webservice.WebServicePath;
 import net.collaud.fablab.webservice.response.OpenDoorResponse;
 
 /**
@@ -21,18 +22,18 @@ import net.collaud.fablab.webservice.response.OpenDoorResponse;
 public class DoorClient {
 	private WebTarget webTarget;
 	private Client client;
-	private static final String BASE_URI = "http://localhost:8080//api";
+	private static final String BASE_URI = "http://localhost:8080/"+WebServicePath.BASE_URL;
 
 	public DoorClient() {
 		client = javax.ws.rs.client.ClientBuilder.newClient();
-		webTarget = client.target(BASE_URI).path("door");
+		webTarget = client.target(BASE_URI).path(WebServicePath.DOOR_URL);
 	}
 
 	public OpenDoorResponse open(String rfid, String token) throws ClientErrorException {
 		WebTarget resource = webTarget;
-		resource = resource.queryParam("rfid", rfid);
-		resource = resource.queryParam("token", token);
-		resource = resource.path("open");
+		resource = resource.queryParam(WebServicePath.PARAM_RFID, rfid);
+		resource = resource.queryParam(WebServicePath.PARAM_TOKEN, token);
+		resource = resource.path(WebServicePath.DOOR_REQUEST_OPEN);
 		return resource.request(MediaType.APPLICATION_XML).get(OpenDoorResponse.class);
 	}
 
@@ -42,7 +43,8 @@ public class DoorClient {
 	
 	public static void main(String[] args) {
 		DoorClient dc = new DoorClient();
-		OpenDoorResponse openResponse = dc.open("6F005C7E99D4", "salut");
+		//OpenDoorResponse openResponse = dc.open("6F005C7E99D4", "salut");
+		OpenDoorResponse openResponse = dc.open("badsfas", "salut");
 		System.out.println("granted : " +openResponse.isGranted());
 		if(openResponse.isError()){
 			System.out.println("error "+openResponse.getErrors().get(0));
