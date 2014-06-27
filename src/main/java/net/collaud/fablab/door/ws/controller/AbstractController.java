77ef3277aa40bbfa0cec3246e5dfbc4f8406a9e6
Gaétan Collaud <gaetancollaud@gmail.com>
2014-06-27 22:57:43 +0200

    Add token for webservice

---
+++ src/main/java/net/collaud/fablab/door/ws/controller/AbstractController.java
@@ -0,0 +1,21 @@
+package net.collaud.fablab.door.ws.controller;
+
+import net.collaud.fablab.common.ws.exception.WebServiceException;
+import net.collaud.fablab.door.file.ConfigFileHelper;
+import net.collaud.fablab.door.file.FileHelperFactory;
+import org.restexpress.Request;
+
+/**
+ *
+ * @author gaetan
+ */
+public class AbstractController {
+
+	protected void checkToken(Request request) throws WebServiceException {
+		String token = request.getQueryStringMap().get("token");
+		String tokenConf = FileHelperFactory.getConfig().get(ConfigFileHelper.WEBSERVICE_TOKEN);
+		if(token==null || !token.equals(tokenConf)){
+			throw new WebServiceException("Invalid token : "+token);
+		}
+	}
+}
